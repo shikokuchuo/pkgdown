a6abe43ed48e58b1de42eba5e5541a5b6c46b46f
Charlie Gao <53399081+shikokuchuo@users.noreply.github.com>
2026-03-02 21:33:39 +0000

    Use nanonext http server for preview (#2975)
    * Use nanonext http server for preview
    
    * Update search docs section
    
    * Add `stop_preview()`
    
    * Add pkgdown reference entry
    
    * Keywords internal `stop_preview()`

--- R/preview.R
+++ R/preview.R
@@ -1,15 +1,16 @@
 #' Open site in browser
 #'
-#' `preview_site()` opens your pkgdown site in your browser. pkgdown has been
-#' carefully designed to work even when served from the file system like
-#' this; the only part that doesn't work is search. You can use `servr::httw("docs/")`
-#'  to create a server to make search work locally.
+#' `preview_site()` opens your pkgdown site in your browser, served via a
+#' local HTTP server. This enables dynamic features such as search to work
+#' correctly in preview.
 #'
+#' @seealso [stop_preview()] to stop the server.
 #' @inheritParams build_article
 #' @param path Path relative to destination
 #' @export
 preview_site <- function(pkg = ".", path = ".", preview = TRUE) {
-  path <- local_path(pkg, path)
+  pkg <- as_pkgdown(pkg)
+  abs_path <- local_path(pkg, path)
 
   check_bool(preview, allow_na = TRUE)
   if (is.na(preview)) {
@@ -17,15 +18,45 @@ preview_site <- function(pkg = ".", path = ".", preview = TRUE) {
   }
 
   if (preview) {
+    root <- pkg$dst_path
+
+    if (is.null(the$server) || !identical(the$server_root, root)) {
+      the$server <- nanonext::http_server(
+        url = "http://127.0.0.1:0",
+        handlers = nanonext::handler_directory("/", root)
+      )
+      the$server$start()
+      the$server_root <- root
+    }
+
     cli::cli_inform(c(i = "Previewing site"))
-    utils::browseURL(path)
+    url <- paste0(the$server$url, "/", if (path != ".") path)
+    utils::browseURL(url)
+  }
+
+  invisible()
+}
+
+#' Stop HTTP preview
+#'
+#' Stops the HTTP server started by [preview_site()], if active. This can be
+#' called manually, but is not strictly necessary as the server is
+#' automatically stopped when previewing a new site or ending the R session.
+#'
+#' @export
+#' @keywords internal
+stop_preview <- function() {
+  if (!is.null(the$server)) {
+    the$server$close()
+    the$server <- NULL
+    the$server_root <- NULL
+    cli::cli_inform(c(i = "Stopped preview"))
   }
 
   invisible()
 }
 
 local_path <- function(pkg, path, call = caller_env()) {
-  pkg <- as_pkgdown(pkg)
   check_string(path, call = call)
 
   abs_path <- path_abs(path, pkg$dst_path)
@@ -33,9 +64,6 @@ local_path <- function(pkg, path, call = caller_env()) {
     cli::cli_abort("Can't find file {.path {path}}.", call = call)
   }
 
-  if (is_dir(abs_path)) {
-    abs_path <- path(abs_path, "index.html")
-  }
   abs_path
 }
 
